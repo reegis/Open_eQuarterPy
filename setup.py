1317993d22d834eb7f43b2a635e7058f5d0fb6ee
uwe <uwe.krien@rl-institut.de>
2016-04-20 17:38:14 +0200

    create setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Open_eQuarterPy',
+      version='0.0.1',
+      author='Uwe Krien',
+      author_email='uwe.krien@rl-institut.de',
+      description='A local heat and power system',
+      package_dir={'Open_eQuarterPy': 'Open_eQuarterPy'},
+      install_requires=['pandas >= 0.18']
+      )
